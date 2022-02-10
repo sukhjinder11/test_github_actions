51e5aacc3d5967c84c8402026ae4380561315073
sukhjinder11 <sukhjinderec@gmail.com>
2022-02-10 15:08:07 +0530

    python script update 7

--- pyscript.py
+++ pyscript.py
@@ -8,8 +8,15 @@ print('current release: ' + current_release)
 cur_rel = current_release.split('v')[1].split('.')
 
 if pr_type == 'bug':
-    cur_rel[2] = 'bug'
+    i = 2
+elif pr_type == 'enhancement':
+    i = 1
+elif pr_type == 'major':
+    i = 0
 
+n = cur_rel[i]
+n = str(int(n) + 1)
+cur_rel[i] = n
 
 next_release = 'v' + cur_rel[0] + '.' + cur_rel[1] + '.' + cur_rel[2]
 print('next_release: ' + next_release)
