deb3a47b7b5a2b1b487c457affb03f7e7e31633e
sukhjinder11 <sukhjinderec@gmail.com>
2022-02-10 14:57:29 +0530

    python script update 5

--- pyscript.py
+++ pyscript.py
@@ -2,8 +2,17 @@ import sys
 
 print('Hello from Python Script')
 pr_type = sys.argv[1]
-current_rel = sys.argv[2].split('v')[1]
+current_release = sys.argv[2]
 
-print(current_rel)
+print('current release: ' + current_release)
+cur_rel = current_release.split('v')[1].split('.')
+
+if pr_type == 'bug':
+    cur_rel[2] = 'bug'
+
+
+next_release = 'v' + cur_rel[0] + '.' + cur_rel[1] + '.' + cur_rel[2]
+
+
 
 
